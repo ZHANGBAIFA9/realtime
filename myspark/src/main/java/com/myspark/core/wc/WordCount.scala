3a375b5c331f5ca1366888d14c6663141007d83c
ZHANGBAIFA9 <1350862999@qq.com>
2021-03-26 10:21:25 +0800

    spark调试之union，对两个rdd不会进行去重操作

--- myspark/src/main/java/com/myspark/core/wc/WordCount.scala
+++ myspark/src/main/java/com/myspark/core/wc/WordCount.scala
@@ -16,13 +16,15 @@ object WordCount {
     //1、读取文件
     val word1s:RDD[String] = sc.textFile("E:\\tmp\\input\\word1.txt")
     val word2s:RDD[String] = sc.textFile("E:\\tmp\\input\\word2.txt")
-
+    //切分
     val word1:RDD[String] = word1s.flatMap(_.split(" "))
     val word2:RDD[String] = word2s.flatMap(_.split(" "))
-
+    //标1成对
     val word1Group:RDD[(String,Int)] = word1.map((_,1))
     val word2Group:RDD[(String,Int)] = word2.map((_,1))
+    //聚合
     var words:RDD[(String,Int)] = word1Group.union(word2Group)
+    //计算
     words.reduceByKey(_+_).collect().foreach(print)
     sc.stop()
   }
